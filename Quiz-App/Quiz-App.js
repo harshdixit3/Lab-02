b4fff31b9838d679aa92f9d87a6cc23ca43b07d5
harshdixit3 <harsh.dixit@3gmail.com>
2024-06-05 05:25:40 +0530

    Creation of fucttions answer options

--- Quiz-App/Quiz-App.js
+++ Quiz-App/Quiz-App.js
@@ -1,4 +1,29 @@
 
 
+function Question (questionId,questionText){
+    this.questionId = questionId;
+    this.questionText = questionText;
+}
 
 const question1 = new Question(1, "Javascript Supports");
+
+function Answer (answerText){
+
+    this.answerText = answerText;
+}
+
+function AnswerOption (AnswerOptionText){
+    this.AnswerOptionText = AnswerOptionText; 
+}
+
+const AnswerOption1 = new AnswerOption("Functions");
+const AnswerOption2 = new AnswerOption("Xhtml");
+const AnswerOption3 = new AnswerOption("Css");
+const AnswerOption4 = new AnswerOption("Html");
+
+
+const correctAnswer1 = new Answer("Fuctions")
+
+const qaCombination1 = new QACombination(
+    question1,correctAnswer1, [AnswerOption1,AnswerOption2,AnswerOption3,AnswerOption4]
+)
