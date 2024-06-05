29799b43a2d7ae3fa4cdee6f260b2d0799cc526b
harshdixit3 <harsh.dixit@3gmail.com>
2024-06-06 03:00:57 +0530

    Created all the question and answere objects are created

--- Quiz-App/Quiz-App.js
+++ Quiz-App/Quiz-App.js
@@ -27,31 +27,62 @@ function AnswerOption(AnswerOptionText) {
 }
 
 function QACombination(questionObj, CurrectAnswerObj, answerOptions) {
-    this.questionObj = questionObj;
-    this.CurrectAnswerObj = CurrectAnswerObj;
-    this.AnswerOption = answerOptions;
-  }
+  this.questionObj = questionObj;
+  this.CurrectAnswerObj = CurrectAnswerObj;
+  this.AnswerOption = answerOptions;
+}
 
 const AnswerOption_Functions = new AnswerOption("Functions");
 const AnswerOption_XHTML = new AnswerOption("Xhtml");
 const AnswerOption_CSS = new AnswerOption("Css");
 const AnswerOption_HTML = new AnswerOption("Html");
 
-
-
 const qaCombination1 = new QACombination(question1, correctAnswer1, [
-    AnswerOption_Functions,
-    AnswerOption_XHTML,
-    AnswerOption_CSS,
-    AnswerOption_HTML,
+  AnswerOption_Functions,
+  AnswerOption_XHTML,
+  AnswerOption_CSS,
+  AnswerOption_HTML,
 ]);
 
 const AnswerOption_JQuary = new AnswerOption("JQuary");
 const AnswerOption_XML = new AnswerOption("XML");
 
 const qaCombination2 = new QACombination(question2, correctAnswer2, [
-    AnswerOption_HTML,
-    AnswerOption_JQuary,
-    AnswerOption_CSS,
-    AnswerOption_XML,
-  ]);
+  AnswerOption_HTML,
+  AnswerOption_JQuary,
+  AnswerOption_CSS,
+  AnswerOption_XML,
+]);
+
+const AnswerOption_Python = new AnswerOption("Python Scriopt");
+const AnswerOption_Django = new AnswerOption("Django");
+const AnswerOption_NodeJS = new AnswerOption("NodeJS");
+
+const qaCombination3 = new QACombination(question3, correctAnswer3, [
+  AnswerOption_Python,
+  AnswerOption_JQuary,
+  AnswerOption_Django,
+  AnswerOption_NodeJS,
+]);
+
+const AnswerOption_PHP = new AnswerOption("PHP");
+const AnswerOption_JS = new AnswerOption("JS");
+const AnswerOption_ALL = new AnswerOption("ALL");
+
+const qaCombination4 = new QACombination(question4, correctAnswer4, [
+  AnswerOption_PHP,
+  AnswerOption_HTML,
+  AnswerOption_JS,
+  AnswerOption_ALL,
+]);
+
+const AnswerOption_Language = new AnswerOption("Language");
+const AnswerOption_ProgramingLanguage = new AnswerOption("Programing Language");
+const AnswerOption_Devlopment = new AnswerOption("Devlopment");
+
+const qaCombination5 = new QACombination(question5, correctAnswer5, [
+  AnswerOption_Language,
+  AnswerOption_ProgramingLanguage,
+  AnswerOption_Devlopment,
+  AnswerOption_ALL,
+]);
